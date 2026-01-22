215b01879b2cbc631e30e8c05ee240860bfee1e8
sahatsawat-pks <sahatsawat.nij@student.mahidol.ac.th>
2026-01-22 11:10:14 +0700

    Fix something

--- 6688249_Sahatsawat/calculator.py
+++ 6688249_Sahatsawat/calculator.py
@@ -22,7 +22,7 @@ def divide(a, b):
     return a / b
 
 def calculate_velocity(distance: float, time: float) -> float:
-    if time <= 0:
+    if time < 0:
         raise ValueError("Time must be greater than zero.")
     return divide(distance, time)
 
